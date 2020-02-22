320b7917256fc5186d365706204b52f4a459f4c7
Charlotte Van Petegem <charlotte@vanpetegem.me>
2020-02-22 18:43:55 +0100

    Add better vim config to accentor web

--- shells/accentor-web.nix
+++ shells/accentor-web.nix
@@ -1,9 +1,32 @@
 let
   pkgs = import <nixpkgs> {};
+  baseVimConfig = import ../programs/neovim/base.nix { inherit pkgs; };
+  nodePackages = import ../packages/node/default.nix { inherit pkgs; };
 in
 pkgs.mkShell {
-  buildInputs = [
-    pkgs.nodejs-12_x
-    pkgs.yarn
+  buildInputs = with pkgs; [
+    nodejs-12_x
+    yarn
+    (neovim.override {
+      configure = {
+          customRC = baseVimConfig.customRC + ''
+            " Required for operations modifying multiple buffers like rename
+            set hidden
+
+            let g:LanguageClient_serverCommands = {
+            \ 'vue': ['${nodePackages.vue-language-server}/bin/vls'],
+            \ }
+          '';
+          vam.knownPlugins = baseVimConfig.vam.knownPlugins;
+          vam.pluginDictionaries = (baseVimConfig.vam.pluginDictionaries or []) ++ [
+            {
+              names = [
+                "LanguageClient-neovim"
+                "vim-vue"
+              ];
+            }
+          ];
+      };
+    })
   ];
 }
