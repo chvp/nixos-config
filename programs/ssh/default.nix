afcabcb99bf7e8e0984fa3089373eedea84b8f24
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2020-02-26 11:43:16 +0100

    Never use nist host keys

--- programs/ssh/default.nix
+++ programs/ssh/default.nix
@@ -7,6 +7,9 @@
       compression = true;
       hashKnownHosts = true;
       serverAliveInterval = 300;
+      extraConfig = ''
+        HostKeyAlgorithms ssh-ed25519-cert-v01@openssh.com,rsa-sha2-512-cert-v01@openssh.com,rsa-sha2-256-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,rsa-sha2-512,rsa-sha2-256,ssh-rsa
+      '';
     };
   };
 }
