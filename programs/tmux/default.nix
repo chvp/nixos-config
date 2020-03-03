6b7a448c05bd4ccc5d171c17e25c8e3b8fa0a0ad
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2020-03-03 12:02:05 +0100

    Tmux settings to not confuse neovim

--- programs/tmux/default.nix
+++ programs/tmux/default.nix
@@ -9,6 +9,9 @@
         bind q kill-session
         bind v run-shell "tmux setw main-pane-width $(($(tmux display -p '#{window_width}') * 70 / 100)); tmux select-layout main-vertical"
         bind h run-shell "tmux setw main-pane-height $(($(tmux display -p '#{window_height}') * 70 / 100)); tmux select-layout main-horizontal"
+
+        set -g default-terminal "screen-256color"
+        set -sg escape-time 10
       '';
       keyMode = "vi";
       tmuxinator.enable = true;
