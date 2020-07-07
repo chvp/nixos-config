789ffd218b6b081987db77b73789d237508d8df6
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2020-07-07 10:16:42 +0200

    Avoid double firefox install

--- overlays/firefox.nix
+++ overlays/firefox.nix
@@ -1,3 +1,5 @@
 self: super: {
   firefox = super.firefox.override { extraNativeMessagingHosts = [ self.passff-host ]; pkcs11Modules = [ self.eid-mw ]; };
+  # Avoids a double firefox install, see https://github.com/NixOS/nixpkgs/pull/31772
+  firefox-bin = self.firefox;
 }
