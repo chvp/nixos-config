732bb8fd7a183a294af35853c2b1035e748a5286
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2020-07-12 11:48:13 +0200

    Don't introduce impurity

--- configurations/zeroad.nix
+++ configurations/zeroad.nix
@@ -5,12 +5,6 @@
     { path = ".config/0ad"; type = "cache"; }
   ];
 
-  nixpkgs.overlays = [
-    (self: super: {
-      zeroadPackages = super.zeroadPackages.override { newScope = extra: self.newScope ({ stdenv = self.stdenvAdapters.impureUseNativeOptimizations self.stdenv; } // extra); };
-    })
-  ];
-
   home-manager.users.charlotte = { pkgs, ... }: {
     home.packages = [ pkgs.zeroad ];
   };
