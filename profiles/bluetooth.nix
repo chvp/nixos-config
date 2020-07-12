ba1798bf3be47653d35f977bb012c65f5899bf1b
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2020-07-12 11:12:29 +0200

    Save bluetooth settings

--- profiles/bluetooth.nix
+++ profiles/bluetooth.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 
 {
+  custom.zfs.systemLinks = [
+    { path = "/var/lib/bluetooth"; type = "cache"; }
+  ];
+
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
   hardware.pulseaudio.extraModules = [ pkgs.pulseaudio-modules-bt ];
