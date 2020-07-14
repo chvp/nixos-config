fbc23062098651f1ad91c0191ada4f21c7373239
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2020-07-14 11:58:05 +0200

    Update dependencies

--- imports/home-manager/metadata.nix
+++ imports/home-manager/metadata.nix
@@ -2,8 +2,8 @@
   url = "https://github.com/rycee/home-manager/archive/master.tar.gz";
   repo_git = "https://github.com/rycee/home-manager";
   branch = "master";
-  rev = "5f189acce44dc39ea4055bfd8064adaf90d7fb5a";
-  sha256 = "0ibmvg3k9m9yzh8ln3jlh47nrvgg81iy8gpl112wjimlp6gagxw6";
-  revdate = "2020-07-04 12:21:02 +0200";
+  rev = "e0fb488e57568d338b987992c4832a83a11badaf";
+  sha256 = "076j1cxx20rg7fhbp8518mgm0wvhm2g7h2cqa727jjrvm6gcs6cj";
+  revdate = "2020-07-14 09:17:05 +0100";
   skip = false;
 }
--- imports/nixpkgs/metadata.nix
+++ imports/nixpkgs/metadata.nix
@@ -2,8 +2,8 @@
   url = "https://github.com/charvp/nixpkgs/archive/master.tar.gz";
   repo_git = "https://github.com/charvp/nixpkgs";
   branch = "master";
-  rev = "f9b5922c9336bebc189c48a07323901ffe1c094a";
-  sha256 = "0kf0rifgas3wyydcpld9q5gwjyxyxgydj0xppkkcgi39whb2gfg5";
-  revdate = "2020-07-13 14:07:55 +0200";
+  rev = "9c6f4a0ed6c09d692bc9ad266a9a4596a36ed353";
+  sha256 = "0ddnzpxmav2v1frmfbj7n2mrw71p61297yargp581r4kay4cfv78";
+  revdate = "2020-07-14 09:53:35 +0200";
   skip = false;
 }
