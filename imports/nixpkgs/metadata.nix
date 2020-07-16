9adc5dfb6fadb262f9ccfd1953c1f03f49fa2670
Charlotte Van Petegem <charlotte@vanpetegem.me>
2020-07-16 22:09:03 +0200

    Update dependencies

--- imports/nixpkgs/metadata.nix
+++ imports/nixpkgs/metadata.nix
@@ -2,8 +2,8 @@
   url = "https://github.com/charvp/nixpkgs/archive/master.tar.gz";
   repo_git = "https://github.com/charvp/nixpkgs";
   branch = "master";
-  rev = "ddf673c855f79a4825cb08d674931806f31daf44";
-  sha256 = "1xgmfg5wx9s3axibv4hnmlhfg4ha6axnr2s2nq5pf0jnlr755cxc";
-  revdate = "2020-07-16 16:00:42 +0200";
+  rev = "600705d252355679c91847c36bf157c79df8401c";
+  sha256 = "0ffa8s3ljn4f5avmn2id4fni455xcz4nf1a7grzdybkw5qlrf0zk";
+  revdate = "2020-07-16 19:18:41 +0200";
   skip = false;
 }
