2eccccfb1116957d58d2c079a80d606c34f7601e
Charlotte Van Petegem <charlotte@vanpetegem.me>
2020-07-22 10:11:18 +0200

    Use even more unstable nixUnstable

--- configurations/nix-store.nix
+++ configurations/nix-store.nix
@@ -5,7 +5,14 @@
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
-    package = pkgs.nixFlakes;
+    package = pkgs.nixUnstable.overrideAttrs (oldAttrs: {
+      src = pkgs.fetchFromGitHub {
+        owner = "NixOS";
+        repo = "nix";
+        rev = "ff314f186e3f91d87af6ad96c0ae3b472494b940";
+        hash = "sha256-QibpLo4/gf2xYGoeQcgjZzH/qy5TBRVH+QCHgqOwur0=";
+      };
+    });
     gc = {
       automatic = true;
       dates = "hourly";
