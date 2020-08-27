ac7a52ae4ab67a24e06e62de66d80c36b3a53daa
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2020-08-27 10:25:07 +0200

    Fix kotlin-ls url

--- packages/kotlin-language-server/default.nix
+++ packages/kotlin-language-server/default.nix
@@ -7,7 +7,7 @@ in
 buildGradle {
   envSpec = ./gradle-env.json;
   src = fetchTarball {
-    url = "https://github.com/fwcd/kotlin-language-server/archive/master.tar.gz";
+    url = "https://github.com/fwcd/kotlin-language-server/archive/0.7.0.tar.gz";
     sha256 = "0by07h2ly84dzmwzjf3fsgghm3fwhyhhbnnv3kl7dy1iajhl4shj";
   };
   gradleFlags = [ "server:installDist" ];
