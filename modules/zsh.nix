9d2e54547cc947f68611c809951c5d01098d36d2
Charlotte Van Petegem <charlotte@vanpetegem.me>
2020-12-02 10:19:54 +0100

    Fix home-manager not running for user

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -51,10 +51,8 @@
       });
     in
     lib.mkIf config.chvp.zsh.enable {
-      chvp.zfs.systemLinks = [
-        { path = "/home/charlotte/.local/share/autojump"; type = "cache"; }
-        { path = "/root/.local/share/autojump"; type = "cache"; }
-      ];
+      chvp.zfs.systemLinks = [ { path = "/root/.local/share/autojump"; type = "cache"; } ];
+      chvp.zfs.homeLinks = [ { path = ".local/share/autojump"; type = "cache"; } ];
       home-manager.users.charlotte = { ... }: (base "/home/charlotte");
       home-manager.users.root = { ... }: (base "/root");
     };
