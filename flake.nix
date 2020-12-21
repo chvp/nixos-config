f042939b5bcbdc7dbdeae44a3ccda92cbe100a5d
Charlotte Van Petegem <charlotte@vanpetegem.me>
2020-12-21 09:41:51 +0100

    Switch to nixos-unstable for a while

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "Nixos configuration flake";
 
   inputs = {
-    nixpkgs = { url = "github:charvp/nixpkgs/master"; };
+    flake-utils.url = "github:numtide/flake-utils/master";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils = { url = "github:numtide/flake-utils/master"; };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, home-manager, flake-utils }:
