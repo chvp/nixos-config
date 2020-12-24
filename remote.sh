eb9d73637a13fb0f49e66123f6271b1c00d8c290
Charlotte Van Petegem <charlotte@vanpetegem.me>
2020-12-24 15:31:48 +0100

    Save remote build results in a profile

--- remote.sh
+++ remote.sh
@@ -2,4 +2,5 @@
 set -euo pipefail
 set -x
 
+nix build --no-link --profile /nix/var/nix/profiles/per-user/charlotte/$1 .#nixosConfigurations.$1.config.system.build.toplevel
 nixos-rebuild --flake .#$1 --target-host root@$1 --build-host localhost $2
