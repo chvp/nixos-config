b5f0ff735dacf24b19c9426a03ad997aa7610726
Charlotte Van Petegem <charlotte@vanpetegem.me>
2021-01-22 14:13:11 +0100

    Use hledger master again

--- configurations/hledger.nix
+++ configurations/hledger.nix
@@ -1,5 +1,23 @@
 { pkgs, ... }:
+let
+  hledger-repo = pkgs.fetchFromGitHub {
+    owner = "simonmichael";
+    repo = "hledger";
+    rev = "540c65994c65c909bf1e365aa0c05671636ac1c7";
+    sha256 = "1sfs6psla1x5243c3kpkhiaskq5jf890lwcqjqbqrrk69il7d8s2";
+  };
+in
 {
+  nixpkgs.overlays = [
+    (self: super: {
+      haskellPackages = super.haskellPackages.override {
+        overrides = hself: hsuper: rec {
+          hledger = hsuper.callCabal2nixWithOptions "hledger" hledger-repo "--subpath hledger" { };
+          hledger-lib = hsuper.callCabal2nixWithOptions "hledger-lib" hledger-repo "--subpath hledger-lib" { };
+        };
+      };
+    })
+  ];
   home-manager.users.charlotte = { pkgs, ... }: {
     home.packages = [ pkgs.hledger ];
   };
