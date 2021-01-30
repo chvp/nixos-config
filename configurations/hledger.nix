5bf2578d6e0439e4a573d97835af0b605a167ec3
Charlotte Van Petegem <charlotte@vanpetegem.me>
2021-01-30 12:53:45 +0100

    Update hledger

--- configurations/hledger.nix
+++ configurations/hledger.nix
@@ -3,8 +3,8 @@ let
   hledger-repo = pkgs.fetchFromGitHub {
     owner = "simonmichael";
     repo = "hledger";
-    rev = "540c65994c65c909bf1e365aa0c05671636ac1c7";
-    sha256 = "1sfs6psla1x5243c3kpkhiaskq5jf890lwcqjqbqrrk69il7d8s2";
+    rev = "31868a6892928c02b385e327083d37f68adc047a";
+    sha256 = "0n7b2vjc8gw10sypgvz4vkw0azgnkw208wpb41sgln95nxfd5dbc";
   };
 in
 {
