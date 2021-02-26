34bfaff5d9d87fcb21c8f557e49c90d855763acc
Charlotte Van Petegem <charlotte@vanpetegem.me>
2021-02-26 10:10:49 +0100

    Build first before needing root privs

--- update.sh
+++ update.sh
@@ -15,7 +15,9 @@ nix flake update --recreate-lock-file
 
 if [ -z "${OVERRIDE:-}" ]
 then
+    nix build --no-link --profile /nix/var/nix/profiles/per-user/charlotte/`hostname` .#nixosConfigurations.`hostname`.config.system.build.toplevel
     su -c "nixos-rebuild --flake . switch"
 else
+    nix build --no-link --override-input nixpkgs ../nixpkgs --no-write-lock-file --profile /nix/var/nix/profiles/per-user/charlotte/`hostname` .#nixosConfigurations.`hostname`.config.system.build.toplevel
     su -c "nixos-rebuild --flake . --override-input nixpkgs ../nixpkgs --no-write-lock-file switch"
 fi
