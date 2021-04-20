9ab2184096768ada46c72495979d8643fbf63cd4
Charlotte Van Petegem <charlotte@vanpetegem.me>
2021-04-20 14:53:36 +0200

    Install zoom again for a while

--- profiles/graphical.nix
+++ profiles/graphical.nix
@@ -45,6 +45,7 @@
       wf-recorder
       wl-clipboard
       youtube-dl
+      zoom-us
     ];
     programs.zsh.loginExtra = ''
       if [[ -z "$DISPLAY" ]] && [[ $(tty) = "/dev/tty1" ]]; then
@@ -61,6 +62,6 @@
 
   chvp = {
     gnupg.pinentryFlavor = "qt";
-    nix.unfreePackages = [ "google-chrome" ];
+    nix.unfreePackages = [ "google-chrome" "zoom" ];
   };
 }
