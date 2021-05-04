e05b51d080b5ad6c970bfa05bb95f2b7caa835ac
Charlotte Van Petegem <charlotte@vanpetegem.me>
2021-05-04 10:45:04 +0200

    Use my own hledger fork for now

--- configurations/hledger.nix
+++ configurations/hledger.nix
@@ -1,6 +1,24 @@
 { pkgs, ... }:
 
+let
+  hledger-repo = pkgs.fetchFromGitHub {
+    owner = "chvp";
+    repo = "hledger";
+    rev = "feature/gain-reports";
+    sha256 = "07qsrq71pnkys11q6k2zc20xc9l3yp8dhzp1ar5bnkgcwbm69rcx";
+  };
+in
 {
+  nixpkgs.overlays = [
+    (self: super: {
+      haskellPackages = super.haskellPackages.override {
+        overrides = hself: hsuper: rec {
+          hledger = hsuper.callCabal2nixWithOptions "hledger" hledger-repo "--subpath hledger" { };
+          hledger-lib = hsuper.callCabal2nixWithOptions "hledger-lib" hledger-repo "--subpath hledger-lib" { };
+        };
+      };
+    })
+  ];
   home-manager.users.charlotte = { pkgs, ... }: {
     home.packages = [ pkgs.hledger ];
   };
