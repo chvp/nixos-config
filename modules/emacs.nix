942da6af275f81d02dcaa54f7d31b0e6fb365f5c
Charlotte Van Petegem <charlotte@vanpetegem.me>
2021-05-15 11:01:18 +0200

    Update dependencies

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -24,12 +24,12 @@ in
     nixpkgs.overlays = [
       (self: super: {
         mu = super.mu.overrideAttrs (old: {
-          version = "1.5.12";
+          version = "1.5.13";
           src = self.fetchFromGitHub {
             owner = "djcb";
             repo = "mu";
-            rev = "c5219778af65dc868eea91c910c801e5105d5cd7";
-            sha256 = "1z6l02bdsh4z2yv3dbixrs0fvyp2axx62dnmqwyampjn5xs5l2yf";
+            rev = "6d67e146fecb5aa512a7eff4b8044225af0dc5ce";
+            sha256 = "0ip7nd7z2l60a3dc1aic34hpab4alb0rmxlk9778nz3v88735iik";
           };
         });
       })
