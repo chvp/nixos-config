226f1501b5a830d65f663f6252933832e85c2dd0
Charlotte Van Petegem <charlotte@vanpetegem.me>
2021-07-28 01:32:14 +0200

    Update dependencies

--- modules/base/emacs/default.nix
+++ modules/base/emacs/default.nix
@@ -25,19 +25,6 @@
   };
 
   config = {
-    nixpkgs.overlays = [
-      (self: super: {
-        mu = super.mu.overrideAttrs (old: {
-          version = "1.5.13";
-          src = self.fetchFromGitHub {
-            owner = "djcb";
-            repo = "mu";
-            rev = "6d67e146fecb5aa512a7eff4b8044225af0dc5ce";
-            sha256 = "0ip7nd7z2l60a3dc1aic34hpab4alb0rmxlk9778nz3v88735iik";
-          };
-        });
-      })
-    ];
     chvp.base.zfs.homeLinks = [
       { path = ".emacs.d"; type = "cache"; }
     ];
