29c39e43118a8c622a1e61a1d12135da0197cfba
Charlotte Van Petegem <charlotte@vanpetegem.me>
2021-07-31 17:02:28 +0200

    Fix desktop file of qutebrowser

--- modules/graphical/xdg/default.nix
+++ modules/graphical/xdg/default.nix
@@ -32,11 +32,11 @@
             "image/jpeg" = [ "org.kde.okular.desktop" ];
             "application/pdf" = [ "org.kde.okular.desktop" ];
 
-            "text/html" = [ "qutebrowser.desktop" ];
-            "x-scheme-handler/about" = [ "qutebrowser.desktop" ];
-            "x-scheme-handler/http" = [ "qutebrowser.desktop" ];
-            "x-scheme-handler/https" = [ "qutebrowser.desktop" ];
-            "x-scheme-handler/unknown" = [ "qutebrowser.desktop" ];
+            "text/html" = [ "org.qutebrowser.qutebrowser.desktop" ];
+            "x-scheme-handler/about" = [ "org.qutebrowser.qutebrowser.desktop" ];
+            "x-scheme-handler/http" = [ "org.qutebrowser.qutebrowser.desktop" ];
+            "x-scheme-handler/https" = [ "org.qutebrowser.qutebrowser.desktop" ];
+            "x-scheme-handler/unknown" = [ "org.qutebrowser.qutebrowser.desktop" ];
 
             "x-scheme-handler/msteams" = [ "teams.desktop" ];
           };
