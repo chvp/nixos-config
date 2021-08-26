9ffb767258d064d270dec59616bd9c2e4f2d0dea
Charlotte Van Petegem <charlotte@vanpetegem.me>
2021-08-26 21:21:38 +0200

    Update dependencies

--- modules/programs/hledger/default.nix
+++ modules/programs/hledger/default.nix
@@ -5,7 +5,7 @@ let
     owner = "simonmichael";
     repo = "hledger";
     rev = "master";
-    sha256 = "yisT9qJYfJNB4nJkFuPXQw1wF/4DmydrjPm+E1wm6Kg=";
+    sha256 = "1+z5i2CoFmkUAGa+WCG89ojZFzzZCKNAuyPZLWX7N18=";
   };
 in
 {
