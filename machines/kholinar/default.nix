f3cfc8e0df0e67c2f2fb7ba5433dca762eb61cde
Charlotte Van Petegem <charlotte@vanpetegem.me>
2021-12-03 14:54:01 +0100

    Add tryhackme vpn

--- machines/kholinar/default.nix
+++ machines/kholinar/default.nix
@@ -1,52 +1,61 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 
 {
   imports = [ ./hardware.nix ];
 
-  networking.hostId = "3cc1a4b2";
+  config = {
+    networking.hostId = "3cc1a4b2";
 
-  time.timeZone = "Europe/Brussels";
+    time.timeZone = "Europe/Brussels";
 
-  # Machine-specific module settings
-  chvp = {
-    stateVersion = "20.09";
-    base = {
-      bluetooth.enable = true;
-      network.mobile = {
-        enable = true;
-        wireless-interface = "wlp0s20f3";
-        wired-interfaces = {
-          "enp0s31f6" = { };
+    services.openvpn.servers.tryhackme = {
+      autoStart = false;
+      config = "config ${config.age.secrets."files/services/openvpn/tryhackme".path}";
+    };
+
+    age.secrets."files/services/openvpn/tryhackme".file = ../../secrets/files/services/openvpn/tryhackme.age;
+
+    # Machine-specific module settings
+    chvp = {
+      stateVersion = "20.09";
+      base = {
+        bluetooth.enable = true;
+        network.mobile = {
+          enable = true;
+          wireless-interface = "wlp0s20f3";
+          wired-interfaces = {
+            "enp0s31f6" = { };
+          };
+        };
+        zfs = {
+          encrypted = true;
+          backups = [
+            {
+              path = "rpool/safe/data";
+              remotePath = "zdata/recv/kholinar/safe/data";
+              fast = true;
+              location = "lasting-integrity";
+            }
+          ];
+          rootDataset = "rpool/local/root";
         };
       };
-      zfs = {
-        encrypted = true;
-        backups = [
-          {
-            path = "rpool/safe/data";
-            remotePath = "zdata/recv/kholinar/safe/data";
-            fast = true;
-            location = "lasting-integrity";
-          }
-        ];
-        rootDataset = "rpool/local/root";
+      development = {
+        enable = true;
+        android.enable = true;
       };
+      games.enable = true;
+      graphical.enable = true;
+      programs = {
+        calibre.enable = false;
+        deluge.enable = true;
+        eid.enable = true;
+        element.enable = true;
+        hledger.enable = true;
+        obs.enable = true;
+      };
+      # It's a pandemic.
+      work.enable = true;
     };
-    development = {
-      enable = true;
-      android.enable = true;
-    };
-    games.enable = true;
-    graphical.enable = true;
-    programs = {
-      calibre.enable = false;
-      deluge.enable = true;
-      eid.enable = true;
-      element.enable = true;
-      hledger.enable = true;
-      obs.enable = true;
-    };
-    # It's a pandemic.
-    work.enable = true;
   };
 }
