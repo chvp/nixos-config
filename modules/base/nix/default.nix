448608c90cdb9ec543e842b9e9a60ce14d22138d
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2022-01-31 10:36:22 +0100

    Update nix trusted users setting name

--- modules/base/nix/default.nix
+++ modules/base/nix/default.nix
@@ -88,7 +88,7 @@ in
         automatic = true;
         dates = [ "hourly" ];
       };
-      trustedUsers = [ "@wheel" ];
+      settings.trusted-users = [ "@wheel" ];
       extraOptions = ''
         substituters = https://cache.nixos.org https://nix-community.cachix.org https://chvp.cachix.org https://accentor.cachix.org
         trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= chvp.cachix.org-1:eIG26KkeA+R3tCpvmaayA9i3KVVL06G+qB5ci4dHBT4= accentor.cachix.org-1:QP+oJwzmeq5Fsyp4Vk501UgUSbl5VIna/ard/XOePH8=
