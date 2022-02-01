5c34fdad62cd123d5f5fa9941c0f19e49af3b845
Charlotte Van Petegem <charlotte@vanpetegem.me>
2022-02-01 16:08:00 +0100

    Override FUP's setting of nixUnstable as default

--- modules/base/nix/default.nix
+++ modules/base/nix/default.nix
@@ -84,6 +84,7 @@ in
         dates = "hourly";
         options = "--delete-older-than 7d";
       };
+      package = pkgs.nixStable;
       settings = {
         auto-optimise-store = true;
         substituters = [
