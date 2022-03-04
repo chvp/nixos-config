d07c6ffeb21d015ecc8f109e31560394cfc9fb8b
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2022-03-04 13:39:36 +0100

    Change authorized keys installation

--- modules/base/sshd/default.nix
+++ modules/base/sshd/default.nix
@@ -10,12 +10,17 @@
       { bits = 4096; path = "${config.chvp.dataPrefix}/etc/ssh/ssh_host_rsa_key"; type = "rsa"; }
       { path = "${config.chvp.dataPrefix}/etc/ssh/ssh_host_ed25519_key"; type = "ed25519"; }
     ];
-    authorizedKeysFiles = [ "/run/agenix/authorized_keys/%u" ];
   };
 
-  age.secrets."authorized_keys/root".file = ../../../secrets/authorized_keys/root.age;
+  age.secrets."authorized_keys/root"= {
+    file = ../../../secrets/authorized_keys/root.age;
+    path = "/root/.ssh/authorized_keys";
+    symlink = false;
+  };
   age.secrets."authorized_keys/charlotte" = {
     file = ../../../secrets/authorized_keys/charlotte.age;
     owner = "charlotte";
+    path = "/home/charlotte/.ssh/authorized_keys";
+    symlink = false;
   };
 }
