0a676146663de21fca515a710d943fefed6842ec
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2022-03-21 11:46:38 +0100

    kotlin-language-server: 1.2.0 -> 1.3.0

--- packages/kotlin-language-server/default.nix
+++ packages/kotlin-language-server/default.nix
@@ -1,11 +1,11 @@
-{ lib, stdenv, fetchzip, jdk11, makeWrapper, ... }:
+{ lib, stdenv, fetchzip, jdk17, makeWrapper, ... }:
 
 stdenv.mkDerivation rec {
   pname = "kotlin-language-server";
-  version = "1.2.0";
+  version = "1.3.0";
   src = fetchzip {
     url = "https://github.com/fwcd/${pname}/releases/download/${version}/server.zip";
-    sha256 = "ooVEYL4ENf15NQWEU/5uQYD+s/RM2vTKDSpWUcccsPM=";
+    sha256 = "nqrZoSntomNLUMTni/yoiVhDOJFSFmZeGze8IJhITu0=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
@@ -14,6 +14,6 @@ stdenv.mkDerivation rec {
     mkdir -p $out
     cp -r $src/* $out/
     chmod -R u=rwX $out
-    wrapProgram $out/bin/kotlin-language-server --set JAVA_HOME ${jdk11}
+    wrapProgram $out/bin/kotlin-language-server --set JAVA_HOME ${jdk17}
   '';
 }
