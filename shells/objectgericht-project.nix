307c22d4a2f57ffeed988a8f63a0b1ca9ff11534
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2022-06-23 17:03:28 +0200

    Python deps for ogprog project

--- shells/objectgericht-project.nix
+++ shells/objectgericht-project.nix
@@ -1,4 +1,4 @@
 { pkgs, ... }: pkgs.devshell.mkShell {
   name = "Objectgericht programmeren project";
-  packages = with pkgs; [ openssl maven openjdk ];
+  packages = with pkgs; [ openssl maven openjdk (python3.withPackages (ps: with ps; [ beautifulsoup4 notmuch pyyaml ])) ];
 }
