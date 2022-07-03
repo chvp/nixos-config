75cba9abe2abd3d9b404696c0697b4388b68c653
Charlotte Van Petegem <charlotte@vanpetegem.me>
2022-07-03 15:39:40 +0200

    jdtls: 1.1.2 -> 1.13.0

--- packages/jdtls/default.nix
+++ packages/jdtls/default.nix
@@ -1,11 +1,11 @@
-{ stdenv, bash, fetchurl, jdk11, ... }:
+{ stdenv, bash, fetchurl, jdk17, ... }:
 
 stdenv.mkDerivation rec {
   pname = "jdtls";
-  version = "1.1.2";
+  version = "1.13.0";
   src = fetchurl {
-    url = "http://download.eclipse.org/${pname}/milestones/${version}/jdt-language-server-${version}-202105191944.tar.gz";
-    sha256 = "1drx4fs63jr1cbsya6smwfx8rxpdlwb9sg3y80xfnr80dh0mh2c4";
+    url = "http://download.eclipse.org/${pname}/milestones/${version}/jdt-language-server-${version}-202206301721.tar.gz";
+    sha256 = "WimtKNdl9AgQaoexR7I5aWlCoR8i+VDFLcNxdvzpz8k=";
   };
   sourceRoot = ".";
   buildPhase = "true";
@@ -19,7 +19,7 @@ stdenv.mkDerivation rec {
     cp -R $out/share/java/jdtls/config_linux \$tmp_dir
     chmod -R u=rwX \$tmp_dir
     trap "{ rm -rf \$tmp_dir; }" 0
-    ${jdk11}/bin/java \
+    ${jdk17}/bin/java \
       -Declipse.application=org.eclipse.jdt.ls.core.id1 \
       -Dosgi.bundles.defaultStartLevel=4 \
       -Declipse.product=org.eclipse.jdt.ls.core.product \
