a4ab6f5301bb0929381cbf7f2b8332845c02ed77
Charlotte Van Petegem <charlotte@vanpetegem.me>
2022-07-09 12:54:38 +0200

    Make sure to always install en_US.UTF-8 locale

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -83,6 +83,11 @@
       extraLocaleSettings = {
         LC_TIME = "en_GB.UTF-8";
       };
+      supportedLocales = [
+        "en_GB.UTF-8/UTF-8"
+        "en_IE.UTF-8/UTF-8"
+        "en_US.UTF-8/UTF-8"
+      ];
     };
 
     security = {
