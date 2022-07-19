62a577fc6a2d09d5e072c3c1a425796163a0c6e5
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2022-07-20 00:29:27 +0200

    Enable avahi for airplay support

--- modules/graphical/sound/default.nix
+++ modules/graphical/sound/default.nix
@@ -19,11 +19,14 @@
     };
 
     sound.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      jack.enable = true;
-      pulse.enable = true;
+    services = {
+      avahi.enable = true;
+      pipewire = {
+        enable = true;
+        alsa.enable = true;
+        jack.enable = true;
+        pulse.enable = true;
+      };
     };
   };
 }
