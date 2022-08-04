b8afb78b393fb3b6d8304a8296e36cb4acc28731
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2022-08-04 10:25:35 +0200

    Use unstable version of hledger

--- modules/programs/hledger/default.nix
+++ modules/programs/hledger/default.nix
@@ -1,5 +1,13 @@
 { config, lib, pkgs, ... }:
 
+let
+  hledger-repo = pkgs.fetchFromGitHub {
+    owner = "simonmichael";
+    repo = "hledger";
+    rev = "4652400f17dcf4fe17d0f7a9eeeff676eb90350c";
+    hash = "sha256-zVzahY22JN1olGDgk/138WTXw2Ojpgt7iKaXsmP4w3M=";
+  };
+in
 {
   options.chvp.programs.hledger.enable = lib.mkOption {
     default = false;
@@ -25,7 +33,14 @@
     ];
 
     home-manager.users.charlotte = { ... }: {
-      home.packages = [ pkgs.hledger ];
+      home.packages = [
+        (pkgs.haskell.lib.justStaticExecutables (pkgs.haskellPackages.override {
+          overrides = hself: hsuper: rec {
+            hledger = hsuper.callCabal2nixWithOptions "hledger" hledger-repo "--subpath hledger" { };
+            hledger-lib = hsuper.callCabal2nixWithOptions "hledger-lib" hledger-repo "--subpath hledger-lib" { };
+          };
+        }).hledger)
+      ];
     };
   };
 }
