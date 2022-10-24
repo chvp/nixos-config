251a337b48c3f3fb3d6e427edb12d9c6d62c2e2d
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2022-10-24 14:07:25 +0200

    Update dependencies

--- modules/services/grafana/default.nix
+++ modules/services/grafana/default.nix
@@ -28,39 +28,42 @@
       };
       grafana = {
         enable = true;
-        analytics.reporting.enable = false;
-        port = 3000;
-        domain = "stats.chvp.be";
-        rootUrl = "https://stats.chvp.be/";
         dataDir = "${config.chvp.dataPrefix}/var/lib/grafana";
-        protocol = "socket";
-        auth.anonymous = {
-          enable = true;
-          org_name = "Van Petegem";
-        };
-        smtp = {
-          enable = true;
-          host = "mail.vanpetegem.me:25";
-          user = "noreply@vanpetegem.me";
-          fromAddress = "noreply@vanpetegem.me";
-          passwordFile = config.age.secrets."passwords/services/grafana/smtp".path;
-        };
-        database = {
-          user = "grafana";
-          type = "postgres";
-          host = "/run/postgresql/";
-          name = "grafana";
-        };
-        users = {
-          allowSignUp = false;
-        };
-        security = {
-          adminUser = "chvp";
-          adminPasswordFile = config.age.secrets."passwords/services/grafana/admin-password".path;
-          secretKeyFile = config.age.secrets."passwords/services/grafana/secret-key".path;
-        };
-        extraOptions = {
-          USERS_DEFAULT_THEME = "light";
+        settings = {
+          analytics.reporting_enabled = false;
+          "auth.anonymous" = {
+            enabled = "true";
+            org_name = "Van Petegem";
+          };
+          database = {
+            user = "grafana";
+            type = "postgres";
+            host = "/run/postgresql/";
+            name = "grafana";
+          };
+          security = {
+            admin_user = "chvp";
+            admin_password = "$__file{${config.age.secrets."passwords/services/grafana/admin-password".path}}";
+            secret_key = "$__file{${config.age.secrets."passwords/services/grafana/secret-key".path}}";
+          };
+          server = {
+            domain = "stats.chvp.be";
+            http_port = 3000;
+            protocol = "socket";
+            root_url = "https://stats.chvp.be";
+            socket = "/run/grafana/grafana.sock";
+          };
+          smtp = {
+            enabled = true;
+            host = "mail.vanpetegem.me:25";
+            user = "noreply@vanpetegem.me";
+            from_address = "noreply@vanpetegem.me";
+            password = "$__file{${config.age.secrets."passwords/services/grafana/smtp".path}}";
+          };
+          users = {
+            default_theme = "light";
+            allow_sign_up = false;
+          };
         };
       };
       grafana-image-renderer = {
