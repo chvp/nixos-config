3e1fc281b0c4d6137434c503e2aea044435e5139
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2022-11-30 09:06:46 +0100

    Update udp2raw derivation

--- packages/udp2raw/default.nix
+++ packages/udp2raw/default.nix
@@ -8,6 +8,7 @@
 stdenv.mkDerivation rec {
   pname = "udp2raw";
   version = "20200818.0";
+
   src = fetchFromGitHub {
     owner = "wangyu-";
     repo = "udp2raw";
@@ -17,10 +18,24 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ makeWrapper ];
 
-  buildPhase = "make dynamic";
+  makeFlags = [ "dynamic" ];
+
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/bin
     cp udp2raw_dynamic $out/bin/udp2raw
     wrapProgram $out/bin/udp2raw --prefix PATH : "${lib.makeBinPath [ iptables ]}"
+
+    runHook postInstall
   '';
+
+  meta = with lib; {
+    homepage = "https://github.com/wangyu-/udp2raw";
+    description = "A tunnel which turns UDP traffic into encrypted UDP/FakeTCP/ICMP traffic by using a raw socket";
+    license = licenses.mit;
+    changelog = "https://github.com/wangyu-/udp2raw/releases/tag/${version}";
+    maintainers = with maintainers; [ chvp ];
+    platforms = platforms.linux;
+  };
 }
