f7afb02ad2bd56d50cd5be0565c3d8c45ae287a2
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2023-01-18 18:21:49 +0100

    Fix local building for remote hosts

--- shells/nixos-config.nix
+++ shells/nixos-config.nix
@@ -1,6 +1,8 @@
 { pkgs, inputs }: pkgs.devshell.mkShell {
   name = "NixOS config";
   packages = [
+    # Use nixos-rebuild from flake, since it might be patched
+    pkgs.nixos-rebuild
     pkgs.nixpkgs-fmt
     (pkgs.writeShellScriptBin "fetchpatch" "curl -L https://github.com/NixOS/nixpkgs/pull/$1.patch -o patches/$1.patch")
     inputs.agenix.defaultPackage.x86_64-linux
