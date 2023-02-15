4b197929873f0d37428fc3c58832da47ed0fb1fb
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2023-02-15 11:37:32 +0100

    jdt-ls is hard to use

--- shells/objectgericht-project.nix
+++ shells/objectgericht-project.nix
@@ -1,4 +1,16 @@
 { pkgs, ... }: pkgs.devshell.mkShell {
   name = "Objectgericht programmeren project";
-  packages = with pkgs; [ openssl maven openjdk17 jdt-language-server ];
+  packages = with pkgs; [
+    openssl maven openjdk17
+  ];
+  commands = [
+    {
+      name = "jdtls";
+      category = "language server";
+      help = "Start jdt-language-server with configured workspace location";
+      command = ''
+        ${pkgs.jdt-language-server}/bin/jdt-language-server -data "$PRJ_DATA_DIR/workspace"
+      '';
+    }
+  ];
 }
