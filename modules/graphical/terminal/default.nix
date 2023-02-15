b69c522501edc2cd9499b2c919ed97b3b11338dc
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2023-02-15 10:52:18 +0100

    Configure terminal further

--- modules/graphical/terminal/default.nix
+++ modules/graphical/terminal/default.nix
@@ -17,8 +17,16 @@
             font = "Hack:size=9";
             dpi-aware = "no";
           };
+          bell = {
+            urgent = true;
+            notify = true;
+          };
           scrollback.lines = 10000;
-          cursor.blink = true;
+          cursor = {
+            blink = true;
+            color = "ffffff 777777";
+          };
+          mouse.hide-when-typing = true;
           colors = {
             foreground = "000000";
             background = "ffffff";
@@ -30,6 +38,14 @@
             regular5 = "721045";
             regular6 = "00538b";
             regular7 = "f8f8f8";
+            bright0 = "000000";
+            bright1 = "972500";
+            bright2 = "315b00";
+            bright3 = "70480f";
+            bright4 = "2544bb";
+            bright5 = "8f0075";
+            bright6 = "30517f";
+            bright7 = "ffffff";
           };
         };
       };
