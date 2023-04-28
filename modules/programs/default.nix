0d839c459af06011053cf10d43b1fa68843696b3
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2023-04-28 11:31:51 +0200

    Update yt-dlp patch checksum

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -16,7 +16,7 @@
         patches = (old.patches or [ ]) ++ [
           (fetchpatch {
             url = https://github.com/yt-dlp/yt-dlp/pull/6654.patch;
-            hash = "sha256-ezJjZyxaSsPfXRoyxL2FQhbO1YUtwhb6mM0atFfO3JY=";
+            hash = "sha256-r7pY7RzF1e2otQmyha6wX3Q9s7H8PllDj1WeriJ2cVs=";
           })
         ];
       }))
