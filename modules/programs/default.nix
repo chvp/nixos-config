40b8b1db30361762219680227c4d891385726bd6
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2023-05-02 15:33:49 +0200

    Switch to yt-dlp again that can download De Mol

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -11,6 +11,22 @@
   ];
 
   home-manager.users.charlotte = { ... }: {
-    home.packages = with pkgs; [ yt-dlp ];
+    home.packages = with pkgs; [
+      (yt-dlp.overrideAttrs (old: {
+        version = "unstable";
+        src = pkgs.fetchFromGitHub {
+          owner = "yt-dlp";
+          repo = "yt-dlp";
+          rev = "b423b6a48e0b19260bc95ab7d72d2138d7f124dc";
+          hash = "sha256-09eELJXIekLGVHjDr9XXPYjsew+ZsSkcAmcMF27mcv8=";
+        };
+        patches = (old.patches or []) ++ [
+          (pkgs.fetchpatch {
+            url = "https://patch-diff.githubusercontent.com/raw/yt-dlp/yt-dlp/pull/6654.diff";
+            hash = "sha256-TNOkhfCf02WZaHyTvlBKBfRre719mdScRJ0azyLTSyQ=";
+          })
+        ];
+      }))
+    ];
   };
 }
