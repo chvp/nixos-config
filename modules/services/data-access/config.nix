795487904554a4e81bac686771899b268d29e9bf
Charlotte Van Petegem <charlotte@vanpetegem.me>
2023-05-15 22:01:23 +0000

    Re-enable "insecure" mac

--- modules/services/data-access/config.nix
+++ modules/services/data-access/config.nix
@@ -28,6 +28,7 @@
     ];
     settings = {
       HostKeyAlgorithms = "+ssh-rsa";
+      Macs = [ "hmac-sha2-512-etm@openssh.com" "hmac-sha2-256-etm@openssh.com" "umac-128-etm@openssh.com" "hmac-sha-512" ];
       PermitRootLogin = "no";
     };
     extraConfig = ''
