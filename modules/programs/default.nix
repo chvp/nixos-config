1673c19d07979c419f0c633d86a4df4506001945
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2023-06-28 09:34:33 +0200

    Update yt-dlp go-play patch

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -15,17 +15,10 @@
       jq
       xsv
       (yt-dlp.overrideAttrs (old: {
-        version = "unstable";
-        src = pkgs.fetchFromGitHub {
-          owner = "yt-dlp";
-          repo = "yt-dlp";
-          rev = "b423b6a48e0b19260bc95ab7d72d2138d7f124dc";
-          hash = "sha256-09eELJXIekLGVHjDr9XXPYjsew+ZsSkcAmcMF27mcv8=";
-        };
         patches = (old.patches or [ ]) ++ [
           (pkgs.fetchpatch {
             url = "https://patch-diff.githubusercontent.com/raw/yt-dlp/yt-dlp/pull/6654.diff";
-            hash = "sha256-TNOkhfCf02WZaHyTvlBKBfRre719mdScRJ0azyLTSyQ=";
+            hash = "sha256-o9T5xGbrP/OgXUGw0s8fiD3+D63leF+OrrLFRBKN7SM=";
           })
         ];
       }))
