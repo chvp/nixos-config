43177d69246ab0df92fbf72498de980caf52b862
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2023-08-28 15:39:05 +0200

    programs: remove patch from yt-dlp

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -14,14 +14,7 @@
     home.packages = with pkgs; [
       jq
       xsv
-      (yt-dlp.overrideAttrs (old: {
-        patches = (old.patches or [ ]) ++ [
-          (pkgs.fetchpatch {
-            url = "https://patch-diff.githubusercontent.com/raw/yt-dlp/yt-dlp/pull/6654.diff";
-            hash = "sha256-o9T5xGbrP/OgXUGw0s8fiD3+D63leF+OrrLFRBKN7SM=";
-          })
-        ];
-      }))
+      yt-dlp
     ];
   };
 }
