dcca3d23699528906691543cdb5edf7950c656cd
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2023-10-27 13:54:52 +0200

    firefox: Use non-deprecated name for native messaging hosts

--- modules/graphical/firefox/default.nix
+++ modules/graphical/firefox/default.nix
@@ -23,7 +23,7 @@ let
     '';
   };
   ffPackage = pkgs.firefox.override {
-    extraNativeMessagingHosts = [ ff2mpv-host ];
+    nativeMessagingHosts = [ ff2mpv-host ];
     pkcs11Modules = [ pkgs.eid-mw ];
     extraPolicies = {
       DisableFirefoxStudies = true;
