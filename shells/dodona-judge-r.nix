f70ab3ac1862f84c958041715a31e2a761b529f6
Charlotte Van Petegem <charlotte@vanpetegem.be>
2024-06-26 16:20:50 +0200

    Remove work-related devshells

--- shells/dodona-judge-r.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }: pkgs.devshell.mkShell {
-  name = "R judge";
-  packages = [
-    (pkgs.rWrapper.override {
-      packages = with pkgs.rPackages; [ base64enc dplyr dslabs jsonlite knitr lintr R6 rlang styler ];
-    })
-  ];
-}
