d9b6df39a16d51601562a1477174bafc7215c797
Charlotte Van Petegem <charlotte@vanpetegem.be>
2024-06-30 20:23:49 +0200

    development: migrate to vue-language-server

--- modules/development/default.nix
+++ modules/development/default.nix
@@ -129,7 +129,7 @@
                                                   :getDocumentPrintWidthRequest nil)
                                                  :documentSymbol t
                                                  :documentColor t)))))
-            (add-to-list 'eglot-server-programs `(vue-mode . ("${pkgs.nodePackages.volar}/bin/vue-language-server" "--stdio" :initializationOptions ,(vue-eglot-init-options))))
+            (add-to-list 'eglot-server-programs `(vue-mode . ("${pkgs.vue-language-server}/bin/vue-language-server" "--stdio" :initializationOptions ,(vue-eglot-init-options))))
             (setq vue--front-tag-lang-regex
               (concat "<%s"                               ; The tag name
                       "\\(?:"                             ; Zero of more of...
