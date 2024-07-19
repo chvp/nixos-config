a3787bb24e98937366880d9dd2d022b7dc9e6278
Charlotte Van Petegem <charlotte@vanpetegem.be>
2024-07-19 16:09:51 +0200

    nix/darwin: enable nix-daemon sandbox

--- modules/darwin/base/nix/default.nix
+++ modules/darwin/base/nix/default.nix
@@ -4,6 +4,7 @@
   nix = {
     configureBuildUsers = true;
     gc.interval = if config.chvp.base.nix.slowGc then { Hour = 0; Minute = 0; } else { Minute = 0; };
+    settings.sandbox = true;
   };
   services.nix-daemon.enable = true;
 }
