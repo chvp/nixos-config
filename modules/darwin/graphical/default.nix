e069d5803bab2e69305c8fcc4755fbe4d5fe7f6e
Charlotte Van Petegem <charlotte@vanpetegem.be>
2024-07-27 11:17:36 +0200

    darwin/graphical: Use open to open emacs

--- modules/darwin/graphical/default.nix
+++ modules/darwin/graphical/default.nix
@@ -5,7 +5,7 @@
     enable = true;
     skhdConfig = ''
       cmd - return : open -na /Applications/iTerm.app
-      cmd + shift - return : emacs
+      cmd + shift - return : open -na ~/Application/Emacs.app
     '';
   };
 }
