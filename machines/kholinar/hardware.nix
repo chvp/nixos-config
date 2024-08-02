ca9f413ce2fcc56ef48dfe1f730caea922f89042
Charlotte Van Petegem <charlotte@vanpetegem.be>
2024-08-02 13:43:56 +0200

    kholinar: reformat

--- machines/kholinar/hardware.nix
+++ machines/kholinar/hardware.nix
@@ -83,12 +83,14 @@
   nixpkgs.overlays = [
     (final: prev: {
       wlroots = prev.wlroots.overrideAttrs (old: {
-        patches = (old.patches or []) ++ [(
-          final.fetchpatch {
-            url = "https://gitlab.freedesktop.org/wlroots/wlroots/uploads/b4f932e370ed03d88f202191eaf60965/DisplayLink.patch";
-            hash = "sha256-1HheLsOSnj4OMiA35QCHkWprTNgAeG2tXrGbaQyUrF4=";
-          }
-        )];
+        patches = (old.patches or [ ]) ++ [
+          (
+            final.fetchpatch {
+              url = "https://gitlab.freedesktop.org/wlroots/wlroots/uploads/b4f932e370ed03d88f202191eaf60965/DisplayLink.patch";
+              hash = "sha256-1HheLsOSnj4OMiA35QCHkWprTNgAeG2tXrGbaQyUrF4=";
+            }
+          )
+        ];
       });
     })
   ];
