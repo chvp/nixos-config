3fb0c9a17913d962085839720ff4de65832c1c48
Charlotte Van Petegem <charlotte@vanpetegem.be>
2024-08-10 12:27:53 +0200

    torrents: downgrade transmission

--- modules/nixos/services/torrents/default.nix
+++ modules/nixos/services/torrents/default.nix
@@ -11,6 +11,21 @@
   config = lib.mkIf config.chvp.services.torrents.enable {
     chvp.services.nginx.hosts = [{ fqdn = "transmission.vanpetegem.me"; basicProxy = "http://localhost:9091"; }];
 
+    nixpkgs.overlays = [
+      (self: super: {
+        transmission_4 = super.transmission_4.overrideAttrs (old: {
+          version = "4.0.5";
+          src = pkgs.fetchFromGitHub {
+            owner = "transmission";
+            repo = "transmission";
+            rev = "4.0.5";
+            hash = "sha256-gd1LGAhMuSyC/19wxkoE2mqVozjGPfupIPGojKY0Hn4=";
+            fetchSubmodules = true;
+          };
+        });
+      })
+    ];
+
     services.transmission = {
       enable = true;
       package = pkgs.transmission_4;
