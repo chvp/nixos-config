2ace9f3275bd892406e0a0b004f1765b02f19659
Charlotte Van Petegem <charlotte@vanpetegem.be>
2024-10-21 11:34:05 +0200

    wireguard: Disable on darwin
    It wasn't really working anyway, and a dependency is failing to build

--- modules/darwin/base/wireguard/default.nix
+++ modules/darwin/base/wireguard/default.nix
@@ -6,20 +6,20 @@ let
   pskFile = config.chvp.base.network.wireguard.pskFile;
 in
 {
-  networking.wg-quick.interfaces."wg0" = {
-    address = [ "${data.${config.networking.hostName}.ip}/32" ];
-    autostart = true;
-    dns = [ data.lasting-integrity.ip ];
-    mtu = 1342;
-    peers = [
-      {
-        allowedIPs = [ subnet ];
-        endpoint = "lasting-integrity.vanpetegem.me:51820";
-        presharedKeyFile = pskFile;
-        persistentKeepalive = 25;
-        publicKey = data.lasting-integrity.pubkey;
-      }
-    ];
-    privateKeyFile = data.${config.networking.hostName}.privkeyFile;
-  };
+  # networking.wg-quick.interfaces."wg0" = {
+  #   address = [ "${data.${config.networking.hostName}.ip}/32" ];
+  #   autostart = true;
+  #   dns = [ data.lasting-integrity.ip ];
+  #   mtu = 1342;
+  #   peers = [
+  #     {
+  #       allowedIPs = [ subnet ];
+  #       endpoint = "lasting-integrity.vanpetegem.me:51820";
+  #       presharedKeyFile = pskFile;
+  #       persistentKeepalive = 25;
+  #       publicKey = data.lasting-integrity.pubkey;
+  #     }
+  #   ];
+  #   privateKeyFile = data.${config.networking.hostName}.privkeyFile;
+  # };
 }
