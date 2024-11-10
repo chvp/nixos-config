49bf8511887ad29072a2f2b090354331f1405649
Charlotte Van Petegem <charlotte@vanpetegem.be>
2024-11-10 22:35:25 +0100

    lasting-integrity: Remove git mount

--- machines/lasting-integrity/hardware.nix
+++ machines/lasting-integrity/hardware.nix
@@ -50,10 +50,6 @@
       device = "zdata/big-apps/nextcloud";
       fsType = "zfs";
     };
-    "/var/lib/git" = {
-      device = "zdata/big-apps/git";
-      fsType = "zfs";
-    };
     "/cache" = {
       device = "zroot/safe/cache";
       fsType = "zfs";
