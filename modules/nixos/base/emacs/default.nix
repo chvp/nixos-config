10c9473187d754d7582f4bf44fafbd644b36b339
Charlotte Van Petegem <charlotte@vanpetegem.be>
2024-11-26 10:59:41 +0100

    emacs: Use emacs-stable on nixos

--- modules/nixos/base/emacs/default.nix
+++ modules/nixos/base/emacs/default.nix
@@ -5,7 +5,7 @@ let
 in
 {
   chvp.base.emacs = {
-    basePackage = pkgs.emacs-pgtk;
+    basePackage = pkgs.emacs29-pgtk;
     extraConfig = [ (builtins.readFile ./linux-init.el) ];
   };
   home-manager.users.${username} = { ... }: {
