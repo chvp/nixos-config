79404f0f2052856ccdaf6fa294fbfdd52251f037
Charlotte Van Petegem <charlotte@vanpetegem.be>
2025-01-13 15:12:15 +0100

    kholinar: Fix evaluation

--- modules/nixos/development/git/default.nix
+++ modules/nixos/development/git/default.nix
@@ -2,10 +2,5 @@
 
 {
   home-manager.users.charlotte.programs.git = lib.mkIf config.chvp.development.git.enable {
-    extraConfig.tag.gpgSign = config.chvp.graphical.enable;
-    signing = {
-      key = "charlotte@vanpetegem.me";
-      signByDefault = config.chvp.graphical.enable;
-    };
   };
 }
