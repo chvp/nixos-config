a2fb17ecdeb69c0b123c483f386e5f9f39c99c14
Charlotte Van Petegem <charlotte@vanpetegem.be>
2025-02-17 09:44:58 +0100

    Fix thaylen-city evaluation

--- modules/darwin/base/nix/default.nix
+++ modules/darwin/base/nix/default.nix
@@ -2,7 +2,7 @@
 
 {
   nix = {
-    configureBuildUsers = true;
+    enable = true;
     gc.interval = if config.chvp.base.nix.slowGc then { Hour = 0; Minute = 0; } else { Minute = 0; };
     optimise.interval = { Minute = 30; };
     settings.sandbox = true;
@@ -11,5 +11,4 @@
     setFlakeRegistry = false;
     setNixPath = false;
   };
-  services.nix-daemon.enable = true;
 }
