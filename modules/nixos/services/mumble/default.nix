08a58e3f88da04cb322315d4241506a1cf5565c8
Charlotte Van Petegem <charlotte@vanpetegem.be>
2025-03-27 09:32:43 +0100

    Configure mumble further

--- modules/nixos/services/mumble/default.nix
+++ modules/nixos/services/mumble/default.nix
@@ -10,9 +10,14 @@
   config = lib.mkIf config.chvp.services.mumble.enable {
     services.murmur = {
       enable = true;
+      environmentFile = config.age.secrets."passwords/services/murmur".path;
       openFirewall = true;
       password = "$MURMURD_PASSWORD";
+      sslKey = "${config.security.acme.certs."vanpetegem.me".directory}/key.pem";
+      sslCert = "${config.security.acme.certs."vanpetegem.me".directory}/cert.pem";
+      sslCa = "${config.security.acme.certs."vanpetegem.me".directory}/chain.pem";
     };
+    users.users.murmur.extraGroups = [ "acme" ];
     age.secrets."passwords/services/murmur" = {
       file = ../../../../secrets/passwords/services/murmur.age;
       owner = "murmur";
