ae71eca94a15924454bff5879395c5362bf57fff
Charlotte Van Petegem <charlotte@vanpetegem.be>
2025-04-30 18:51:29 +0200

    Replace substituteAll with replaceVars

--- modules/nixos/graphical/compositor/launcher.nix
+++ modules/nixos/graphical/compositor/launcher.nix
@@ -1,20 +1,14 @@
 { pkgs, stdenv }:
 let
-  script = pkgs.substituteAll {
-    src = ./launcher.zsh;
+  script = pkgs.replaceVars ./launcher.zsh {
     inherit (pkgs)
       fzy
-      jq
       libqalculate
       nix
-      pass
-      slurp
       uni
       zsh
       ;
-    # wfRecorder = pkgs.wf-recorder;
     wlClipboard = pkgs.wl-clipboard;
-    xdgUserDirs = pkgs.xdg-user-dirs;
   };
 in
 pkgs.runCommand "launcher" { } ''
