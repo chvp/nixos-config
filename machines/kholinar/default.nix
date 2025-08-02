92170d03a18250d2ce0800be985598f50727565b
Charlotte Van Petegem <charlotte@vanpetegem.be>
2025-08-02 10:48:28 +0200

    Use latest zfs-compatible kernel on kholinar

--- machines/kholinar/default.nix
+++ machines/kholinar/default.nix
@@ -1,7 +1,22 @@
 { pkgs, lib, config, ... }:
 
+let
+zfsCompatibleKernelPackages = lib.filterAttrs (
+    name: kernelPackages:
+    (builtins.match "linux_[0-9]+_[0-9]+" name) != null
+    && (builtins.tryEval kernelPackages).success
+    && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
+  ) pkgs.linuxKernel.packages;
+  latestKernelPackage = lib.last (
+    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
+      builtins.attrValues zfsCompatibleKernelPackages
+    )
+  );
+in
 {
   imports = [ ./hardware.nix ];
+
+  boot.kernelPackages = latestKernelPackage;
 
   networking.hostId = "6008fa3f";
 
