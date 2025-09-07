1e315937e86fcd09e46073d91c4c9d0d9dc8591f
Charlotte Van Petegem <charlotte@vanpetegem.be>
2025-09-07 15:47:13 +0200

    Setup cachix for charlotte

--- modules/nixos/base/nix/default.nix
+++ modules/nixos/base/nix/default.nix
@@ -3,4 +3,12 @@
   nix.gc.dates = if config.chvp.base.nix.slowGc then "daily" else "hourly";
   nix.optimise.dates = [ "hourly" ];
   programs.command-not-found.enable = false;
+
+  chvp.base.zfs.homeLinks = [
+    { path = ".config/cachix"; type = "cache"; }
+  ];
+
+  home-manager.users.charlotte = { ... }: {
+    home.packages = [ pkgs.cachix ];
+  };
 }
