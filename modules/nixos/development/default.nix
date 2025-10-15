df40d8767d7be1694e548685fb12b0cb775d3325
Charlotte Van Petegem <charlotte@vanpetegem.be>
2025-10-15 13:42:14 +0200

    More virtualbox settings

--- modules/nixos/development/default.nix
+++ modules/nixos/development/default.nix
@@ -9,13 +9,15 @@
   config = lib.mkIf config.chvp.development.enable {
     chvp = {
       base = {
-        nix.unfreePackages = [ "ruby-mine" ];
+        nix.unfreePackages = [ "ruby-mine" "Oracle_VirtualBox_Extension_Pack" ];
         zfs.homeLinks = [
           { path = "repos"; type = "cache"; }
           { path = ".config/JetBrains"; type = "cache"; }
           { path = ".local/share/JetBrains"; type = "cache"; }
           { path = ".cache/JetBrains"; type = "cache"; }
           { path = ".java/.userPrefs"; type = "cache"; }
+          { path = "VirtualBox VMs"; type = "cache"; }
+          { path = ".config/VirtualBox"; type = "cache"; }
         ];
       };
       development.docker.enable = lib.mkDefault true;
@@ -33,6 +35,9 @@
 
     users.users.charlotte.extraGroups = [ "vboxusers" ];
 
-    virtualisation.virtualbox.host.enable = true;
+    virtualisation.virtualbox.host = {
+      enable = true;
+      enableExtensionPack = true;
+    };
   };
 }
