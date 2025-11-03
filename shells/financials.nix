d43f6746bfc1d80ed01e7abcee8ab46f49c3f0e7
Charlotte Van Petegem <charlotte@vanpetegem.be>
2025-11-03 16:00:18 +0100

    Update financials shell

--- shells/financials.nix
+++ shells/financials.nix
@@ -3,7 +3,7 @@
 pkgs.devshell.mkShell {
   name = "Financials";
   packages = with pkgs; [
-    (python3.withPackages (ps: [ ps.requests ps.dateutil ]))
+    (python3.withPackages (ps: [ ps.requests ps.python-dateutil ]))
     hledger
   ];
 }
