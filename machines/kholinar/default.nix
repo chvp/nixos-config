2bddfc6ef21900afb6ab88113bf521ea38a8bb2f
Charlotte Van Petegem <charlotte@vanpetegem.be>
2025-11-19 10:23:40 +0100

    Simplify usage of latest linux kernel

--- machines/kholinar/default.nix
+++ machines/kholinar/default.nix
@@ -1,24 +1,9 @@
 { pkgs, lib, config, ... }:
 
-let
-  zfsCompatibleKernelPackages = lib.filterAttrs
-    (
-      name: kernelPackages:
-        (builtins.match "linux_[0-9]+_[0-9]+" name) != null
-        && (builtins.tryEval kernelPackages).success
-        && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
-    )
-    pkgs.linuxKernel.packages;
-  latestKernelPackage = lib.last (
-    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
-      builtins.attrValues zfsCompatibleKernelPackages
-    )
-  );
-in
 {
   imports = [ ./hardware.nix ];
 
-  boot.kernelPackages = lib.mkForce latestKernelPackage;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.zfs.package = pkgs.zfs_unstable;
 
   networking.hostId = "6008fa3f";
