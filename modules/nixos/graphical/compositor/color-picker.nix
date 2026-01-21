1a1e352d3fb3132c421f6de3ba6ac2b3b5552371
Charlotte Van Petegem <charlotte@vanpetegem.be>
2026-01-21 14:09:23 +0100

    Use riverctl in color picker

--- modules/nixos/graphical/compositor/color-picker.nix
+++ modules/nixos/graphical/compositor/color-picker.nix
@@ -3,5 +3,5 @@
 pkgs.writeShellScriptBin "color_picker" ''
   color=$(${pkgs.grim}/bin/grim -t png -g "$(${pkgs.slurp}/bin/slurp -p)" - | ${pkgs.imagemagick}/bin/convert png:- -unique-colors txt:- | grep -o '#[A-F0-9]\+')
 
-  ${pkgs.sway}/bin/swaymsg exec -- "echo -n '$color' | ${pkgs.wl-clipboard}/bin/wl-copy --foreground"
+  ${pkgs.river-classic}/bin/riverctl spawn "echo -n '$color' | ${pkgs.wl-clipboard}/bin/wl-copy --foreground"
 ''
