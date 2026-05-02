43ca4ab5708674f4689b21ece5e877c28ad9df0d
Charlotte Van Petegem <charlotte@vanpetegem.be>
2026-05-02 22:27:19 +0200

    Change remote script cache to push to attic

--- remote.sh
+++ remote.sh
@@ -8,7 +8,7 @@ shift 1
 if [ "$1" == "cache" ]
 then
     nix build -L --no-link .#nixosConfigurations.$hostname.config.system.build.toplevel
-    nix eval --json ".#nixosConfigurations.$hostname.config.system.build.toplevel.outPath" | sed 's/"\(.*\)"/\1/' | cachix push chvp
+    nix eval --json ".#nixosConfigurations.$hostname.config.system.build.toplevel.outPath" | sed 's/"\(.*\)"/\1/' | attic push chvp --stdin
 else
     nixos-rebuild --flake .#$hostname --target-host root@$hostname -s "$@"
 fi
