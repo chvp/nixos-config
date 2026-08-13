7d8e9d924f02cacfabda2a834c44da5552ed23e4
Charlotte Van Petegem <charlotte@vanpetegem.be>
2026-08-13 11:04:45 +0200

    Re-enable calibre with ffmpeg pinned to version 8

--- modules/programs/calibre/default.nix
+++ modules/programs/calibre/default.nix
@@ -11,7 +11,7 @@
       { path = ".config/calibre"; type = "cache"; }
     ];
     home-manager.users.charlotte = { ... }: {
-      # home.packages = [ pkgs.calibre ];
+      home.packages = [ (pkgs.calibre.override { ffmpeg = pkgs.ffmpeg_8; }) ];
     };
     services.udisks2.enable = true;
   };
