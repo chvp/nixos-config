79d3b670a348b6738c39fb8fc690a384ba2f2b7a
Charlotte Van Petegem <charlotte@vanpetegem.be>
2026-08-29 16:53:12 +0200

    Allow disabling root ssh login

--- modules/base/sshd/default.nix
+++ modules/base/sshd/default.nix
@@ -4,38 +4,45 @@ let
   username = config.chvp.username;
 in
 {
-  chvp.base.zfs = {
-    ensureSystemExists = [ "${config.chvp.dataPrefix}/etc/ssh" ];
-    ensureHomeExists = [ ".ssh" ];
-  };
-  services.openssh = {
-    enable = true;
-    hostKeys = [
-      {
-        bits = 4096;
-        path = "${config.chvp.dataPrefix}/etc/ssh/ssh_host_rsa_key";
-        type = "rsa";
-      }
-      {
-        path = "${config.chvp.dataPrefix}/etc/ssh/ssh_host_ed25519_key";
-        type = "ed25519";
-      }
-    ];
-    settings = {
-      PasswordAuthentication = false;
-      PermitRootLogin = "prohibit-password";
-    };
+  options.chvp.base.ssh.allowRootLogin = lib.mkOption {
+    default = true;
+    example = false;
   };
 
-  age.secrets."authorized_keys/root" = {
-    file = ../../../secrets/authorized_keys/root.age;
-    path = "/root/.ssh/authorized_keys";
-    symlink = false;
-  };
-  age.secrets."authorized_keys/charlotte" = {
-    file = ../../../secrets/authorized_keys/charlotte.age;
-    owner = username;
-    path = "/home/${username}/.ssh/authorized_keys";
-    symlink = false;
+  config = {
+    chvp.base.zfs = {
+      ensureSystemExists = [ "${config.chvp.dataPrefix}/etc/ssh" ];
+      ensureHomeExists = [ ".ssh" ];
+    };
+    services.openssh = {
+      enable = true;
+      hostKeys = [
+        {
+          bits = 4096;
+          path = "${config.chvp.dataPrefix}/etc/ssh/ssh_host_rsa_key";
+          type = "rsa";
+        }
+        {
+          path = "${config.chvp.dataPrefix}/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+      ];
+      settings = {
+        PasswordAuthentication = false;
+        PermitRootLogin = if config.chvp.base.ssh.allowRootLogin then "prohibit-password" else "no";
+      };
+    };
+
+    age.secrets."authorized_keys/root" = {
+      file = ../../../secrets/authorized_keys/root.age;
+      path = "/root/.ssh/authorized_keys";
+      symlink = false;
+    };
+    age.secrets."authorized_keys/charlotte" = {
+      file = ../../../secrets/authorized_keys/charlotte.age;
+      owner = username;
+      path = "/home/${username}/.ssh/authorized_keys";
+      symlink = false;
+    };
   };
 }
