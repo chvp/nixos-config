61784eaa92525e3dda0e2a5a94d66459e2d7cf93
Charlotte Van Petegem <charlotte@vanpetegem.be>
2026-09-02 22:31:45 +0200

    Use my personal prefix in early-init.el

--- modules/base/emacs/early-init.el
+++ modules/base/emacs/early-init.el
@@ -1,12 +1,12 @@
 ;;; early-init --- My emacs early init file
 ;;; Commentary:
 ;;; Code:
-(defun hm/reduce-gc ()
+(defun chvp--reduce-gc ()
   "Reduce the frequency of garbage collection."
   (setq gc-cons-threshold most-positive-fixnum
         gc-cons-percentage 0.6))
 
-(defun hm/restore-gc ()
+(defun chvp--restore-gc ()
   "Restore the frequency of garbage collection."
   (setq gc-cons-threshold 16777216
         gc-cons-percentage 0.1))
@@ -14,13 +14,13 @@
 ;; Make GC more rare during init, while minibuffer is active, and
 ;; when shutting down. In the latter two cases we try doing the
 ;; reduction early in the hook.
-(hm/reduce-gc)
-(add-hook 'minibuffer-setup-hook #'hm/reduce-gc -50)
-(add-hook 'kill-emacs-hook #'hm/reduce-gc -50)
+(chvp--reduce-gc)
+(add-hook 'minibuffer-setup-hook #'chvp--reduce-gc -50)
+(add-hook 'kill-emacs-hook #'chvp--reduce-gc -50)
 
 ;; But make it more regular after startup and after closing minibuffer.
-(add-hook 'emacs-startup-hook #'hm/restore-gc)
-(add-hook 'minibuffer-exit-hook #'hm/restore-gc)
+(add-hook 'emacs-startup-hook #'chvp--restore-gc)
+(add-hook 'minibuffer-exit-hook #'chvp--restore-gc)
 
 ;; Nix manages our packages
 (setq package-enable-at-startup nil)
